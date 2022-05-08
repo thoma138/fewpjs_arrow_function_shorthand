236658ff1f58b416364ba98d42263c1ad68a219b
thoma138 <thomaselliott@live.com>
2022-05-08 12:17:50 +1000

    commit tree c6dec7659a60db66d41a8b0898cdbdcedebaa46c

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function(paramater1 = "2000", parameter2 = "100") {
+    return paramater1/parameter2;
+}
+
+let square = (x = "2") => x ** x;
+
+let add = (a = '3', b = '4') => a + b;
+
+
